33564cd770120062c35f3228817d9bdc28b8c8f3
Claudia Simone De Souza <110047472+ClaudiaSouza1812@users.noreply.github.com>
2024-09-10 17:26:24 +0100

    240910

--- Exercises/Exe10_NumberOfWords.js
+++ Exercises/Exe10_NumberOfWords.js
@@ -1,3 +1,20 @@
 function numberOfWords(sentence) {
     let words = sentence.split(" ");
+    return words.length;
 }
+
+console.log(numberOfWords("Example of a sentence with words"));
+
+function numberOfWords02(sentence) {
+    
+    let numberOfWords = 1;
+
+    for (let character of sentence) {
+        if (character !== " ") {
+            continue; 
+        }
+        numberOfWords++; 
+    }
+    return numberOfWords;
+};
+console.log(numberOfWords02("Example of a sentence, with words!"));
