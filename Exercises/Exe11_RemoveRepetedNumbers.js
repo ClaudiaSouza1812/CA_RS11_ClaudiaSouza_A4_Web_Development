79287617657bb3c5281a927a83e5dcfbc65cb351
Claudia Simone De Souza <110047472+ClaudiaSouza1812@users.noreply.github.com>
2024-09-10 17:58:03 +0100

    240910

---
+++ Exercises/Exe11_RemoveRepetedNumbers.js
@@ -0,0 +1,13 @@
+let numbers = [12,6,3,12,8,6,5];
+
+for (let i = 0, j = i + 1; i < numbers.length - 1; j++) {
+    if (numbers[i] === numbers[j]) {
+        numbers.splice(j, 1);
+    }
+    if (j == numbers.length - 1) {
+        i++;
+        j = i;
+    }
+}
+
+console.log(numbers); // [12, 6, 3, 8, 5]
