a21b8062ab383e0b05045b65e8fdcb53058bd681
Claudia Simone De Souza <110047472+ClaudiaSouza1812@users.noreply.github.com>
2024-09-18 15:59:44 +0100

    240918

--- Exercises/Exe19_TheHangedGame.js
+++ Exercises/Exe19_TheHangedGame.js
@@ -15,11 +15,11 @@ words = [
     "kaleidoscope", "lighthouse", "magnificent", "negotiation", "opportunity",
     // 12 letters
     "photography", "quarterback", "reservation", "subscription", "temperature"
-  ];
+];
 
-  let choosedWord = words[Math.floor(Math.random() * 40)];
+let choosedWord = words[Math.floor(Math.random() * 40)];
 
-  function setAttempts(word) {
+function setAttempts(word) {
     let attempts = 0;
     if (word.length === 5) {
         attempts = 5;
@@ -29,40 +29,63 @@ words = [
         attempts = 3;
     }
     return attempts;
-  }
+}
 
-  function runHangedGame(word) {
+function capitalizeFirstLetter(word) {
+    word[0] = word[0].toUpperCase();
+    return word;
+}
+
+function getPlayerLetter() {
+    let playerLetter = "";
+    while (playerLetter.length !== 1) {
+        playerLetter = prompt("Type a letter: ").toLowerCase();
+    }
+    return playerLetter;
+}
+
+function checkPlayerLetter(playerLetter) {
+    return choosedWord.includes(playerLetter);
+}
+
+function decryptWord(maskedWord, playerLetter) {
+    maskedWord = choosedWord.map(letter => letter === playerLetter ? letter : maskedWord[choosedWord.indexOf(letter)]);
+    if (maskedWord[0].charCodeAt(0) >= 97 && maskedWord[0].charCodeAt(0) <= 122) {
+        maskedWord = capitalizeFirstLetter(maskedWord);
+    }
+    return maskedWord;
+}
+
+function runHangedGame(word) {
+
     let choosedWord = word.split("");
     let attempts = setAttempts(choosedWord);
     let maskedWord = choosedWord.map(() => "_");
     let alreadyWon = false;
+
+    console.log(`Try to discover the word before you run out of attempts: (${attempts}).`);
     console.log(maskedWord.join(" "));
 
     while (attempts > 0 || alreadyWon) {
-        if (choosedWord.includes("_")) {
-            let playerLetter = "";
-            while (playerLetter.length !== 1) {
-                playerLetter = prompt("Type a letter: ").toLowerCase();
-            }
+        if (maskedWord.includes("_")) {
+            let playerLetter = getPlayerLetter();
 
-            if (choosedWord.includes(playerLetter)) {
-                maskedWord = choosedWord.map(letter => letter === playerLetter ? letter : "_ ")
-                console.log(maskedWord);
+            if (checkPlayerLetter(playerLetter)) {
+                maskedWord = decryptWord(maskedWord, playerLetter);
+                console.log(maskedWord.join(" "));
             } else {
-                console.log(`The letter ${playerLetter} is not in the word`);
                 attempts -= 1;
-                console.log(maskedWord);
+                console.log(`The letter "${playerLetter}" is not in the word. Attempts left: (${attempts}).`);
+                console.log(maskedWord.join(" "));
             }
         } else {
             alreadyWon = true;
-            console.log(`The word is ${word}, congrats, you won!!!`);
-            break;
+            console.log(`The word is "${maskedWord}", congrats, you won!!!`);
         }
     }
-    if (attempts === 0 &&!alreadyWon) {
-    console.log(`The word was ${word}, better luck next time!`);
+    if (attempts === 0) {
+    console.log(`The word was "${capitalizeFirstLetter(word)}", better luck next time!`);
     }
-    
-  }
+}
 
-  runHangedGame(choosedWord);
+runHangedGame(choosedWord);
